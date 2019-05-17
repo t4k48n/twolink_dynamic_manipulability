6f10be7cf5f55d8f2ebed1fbb25beb7430822c80
Hideaki TAKATANI <ev18d001@steng.u-hyogo.ac.jp>
2019-05-17 21:50:58 +0900

    Update

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
-.PHONY: all clean
+.PHONY: all run clean
 
 all: main test
 
 main: utils.ml model.ml main.ml
-	ocamlfind ocamlopt -o $@ $^
+	ocamlfind ocamlopt -package unix -linkpkg -o $@ $^
 
 test: utils.ml model.ml test.ml
 	ocamlfind ocamlopt -o $@ $^
+
+run: main
+	./main
 
 clean:
 	rm -f *.cmi *.cmx *.o main test
--- main.ml
+++ main.ml
@@ -3,8 +3,10 @@ open Utils
 open Model
 
 let postures =
-  let ticks = list_linspace 0.0 (2.0 *. pi) 21 in
-  List.concat @@ List.map (fun q1 -> List.map (fun q2 -> (q1, q2)) ticks) ticks
+  let ticks = list_linspace 0.0 (2.0 *. pi) 361 in
+  (* List.concat @@ List.map (fun q1 -> List.map (fun q2 -> (q1, q2)) ticks) ticks *)
+  let q1 = pi /. 4.0 in
+  List.map (fun q2 -> (q1, q2)) ticks
 
 let uncertainties =
   let ticks = list_linspace (-1.0) 1.0 21 in
@@ -12,9 +14,29 @@ let uncertainties =
 
 (* MC法で求めた各姿勢における動的可操作性 *)
 let dynamic_manipulabilities_with_monte_carlo =
-  List.map (fun p -> List.map (fun z -> dynamic_manipulability_with_grasp p z 0.5) uncertainties) postures
+  List.map (fun p -> List.map (fun z -> dynamic_manipulability_with_grasp p z 1.0) uncertainties) postures
 
 let () =
   let dynman_means = List.map list_mean dynamic_manipulabilities_with_monte_carlo in
   let dynman_variances = List.map list_variance dynamic_manipulabilities_with_monte_carlo in
-  List.iter2 (fun m v -> Printf.printf "%f,%f\n" m v) dynman_means dynman_variances
+  let lines_to_be_printed = List.combine postures @@ List.combine dynman_means dynman_variances in
+  let b = Buffer.create 0 in
+  Buffer.add_string b
+    "import numpy\n\
+     import matplotlib.pyplot as pyplot\n\
+     data = numpy.matrix('";
+  List.iter (fun ((q1, q2), (m, v)) -> Buffer.add_string b (Printf.sprintf "%f %f %f %f;" q1 q2 m v)) lines_to_be_printed;
+  Buffer.add_string b
+    "'[:-1])\n\
+     pyplot.subplot(311)\n\
+     pyplot.plot(data[:,1], data[:,2])\n\
+     pyplot.subplot(312)\n\
+     pyplot.plot(data[:,1], data[:,3])\n\
+     pyplot.subplot(313)\n\
+     pyplot.plot(data[:,1], data[:,2]/data[:,3])\n\
+     pyplot.show()\n\
+     ";
+  let pyf = open_out "main.ml.py" in
+  Printf.fprintf pyf "%s" (Buffer.contents b);
+  close_out pyf;
+  let _ = Unix.system "pipenv run python3 main.ml.py" in ()
